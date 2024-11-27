d913aa72e10cdbc643017ea2ea720e0262a6b22c
3cjosevalter <00001083306558sp@al.educacao.sp.gov.br>
2024-11-27 17:37:48 -0300

    Add files via upload

---
+++ alura/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Valorant',
+    'Quantos agentes existem no Valorant?',
+    'Atualmente, Valorant possui 22 agentes disponíveis.'
+);
+
+criaCartao(
+    'Valorant',
+    'Qual é o objetivo principal no modo padrão do Valorant?',
+    'O objetivo principal é plantar ou desarmar a Spike, dependendo do lado em que você está (atacante ou defensor).'
+);
+
+criaCartao(
+    'Valorant',
+    'O que é uma ultimate no Valorant?',
+    'A ultimate é a habilidade mais poderosa de cada agente, carregada com base em eliminações, orbes ou rodadas jogadas.'
+);
+
+criaCartao(
+    'Valorant',
+    'Quais são os papéis dos agentes no Valorant?',
+    'Os papéis incluem Duelista, Controlador, Iniciador e Sentinela, cada um com funções específicas na equipe.'
+);
